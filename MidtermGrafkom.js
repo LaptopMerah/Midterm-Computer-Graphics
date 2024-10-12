4e2cad99f93479373570ae11d423fc59e2932330
LaptopMerah” <iqballramadhan18@gmail.com>
2024-10-12 20:40:47 +0700

    feat: update v1

--- MidtermGrafkom.js
+++ MidtermGrafkom.js
@@ -4,122 +4,87 @@ var MidtermGrafkom = function () {
   var canvas;
   var gl;
 
-  var numPositions = 108;
+  var numPositions = 36;
 
   var positionsArray = [];
+  var normalsArray = [];
   var colorsArray = [];
 
-  const A = (1 + Math.sqrt(5)) / 2; // The golden ratio
-  const B = 1 / A;
-
   var vertices = [
-    vec4(1, 1, 1, 1.0),
-    vec4(1, 1, -1, 1.0),
-    vec4(1, -1, 1, 1.0),
-    vec4(1, -1, -1, 1.0),
-    vec4(-1, 1, 1, 1.0),
-    vec4(-1, 1, -1, 1.0),
-    vec4(-1, -1, 1, 1.0),
-    vec4(-1, -1, -1, 1.0),
-    vec4(0, B, A, 1.0),
-    vec4(0, B, -A, 1.0),
-    vec4(0, -B, A, 1.0),
-    vec4(0, -B, -A, 1.0),
-    vec4(B, A, 0, 1.0),
-    vec4(B, -A, 0, 1.0),
-    vec4(-B, A, 0, 1.0),
-    vec4(-B, -A, 0, 1.0),
-    vec4(A, 0, B, 1.0),
-    vec4(A, 0, -B, 1.0),
-    vec4(-A, 0, B, 1.0),
-    vec4(-A, 0, -B, 1.0),
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0),
   ];
 
-  for (var i = 0; i < vertices.length; i++) {
-    vertices[i][0] /= 2; // Scale the x component
-    vertices[i][1] /= 2; // Scale the y component
-    vertices[i][2] /= 2; // Scale the z component
-  }
+  var objectColor = vec4(1.0, 1.0, 1.0, 0.0);
 
-  var vertexColors = [
-    vec4(0.0, 0.0, 0.0, 1.0), // black
-    vec4(1.0, 0.0, 0.0, 1.0), // red
-    vec4(1.0, 1.0, 0.0, 1.0), // yellow
-    vec4(0.0, 1.0, 0.0, 1.0), // green
-    vec4(0.0, 0.0, 1.0, 1.0), // blue
-    vec4(1.0, 0.0, 1.0, 1.0), // magenta
-    vec4(0.0, 1.0, 1.0, 1.0), // cyan
-    vec4(1.0, 1.0, 1.0, 1.0), // white
-    vec4(0.5, 0.5, 0.5, 1.0), // gray
-    vec4(1.0, 0.5, 0.0, 1.0), // orange
-    vec4(0.5, 0.0, 0.5, 1.0), // purple
-    vec4(0.5, 1.0, 0.5, 1.0), // light green
-    vec4(0.5, 0.5, 1.0, 1.0), // light blue
-    vec4(1.0, 0.5, 1.0, 1.0), // pink
-    vec4(0.0, 0.5, 1.0, 1.0), // sky blue
-    vec4(1.0, 1.0, 0.5, 1.0), // light yellow
-    vec4(0.0, 1.0, 0.5, 1.0), // teal
-    vec4(0.5, 0.0, 0.0, 1.0), // dark red
-    vec4(0.0, 0.5, 0.0, 1.0), // dark green
-    vec4(0.0, 0.0, 0.5, 1.0), // dark blue
-  ];
+  var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
+  var lightAmbient = vec4(0.5, 0.5, 0.5, 1.0);
+  var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+  var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-  var near = 0.3;
-  var far = 3.0;
-  var radius = 2.0;
-  var theta = 0.0;
-  var phi = 0.0;
-  var dr = (5.0 * Math.PI) / 180.0;
+  var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
+  var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
+  var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
+  var materialShininess = 20.0;
 
-  var fovy = 60.0; // Field-of-view in Y direction angle (in degrees)
-  var aspect; // Viewport aspect ratio
-
-  var modelViewMatrixLoc, projectionMatrixLoc;
+  var ctm;
+  var ambientColor, diffuseColor, specularColor;
   var modelViewMatrix, projectionMatrix;
-  var eye;
-  const at = vec3(0.0, 0.0, 0.0);
-  const up = vec3(0.0, 1.0, 0.0);
+  var viewerPos;
+  var program;
+  var aspect;
 
-  function dedocahedron(a, b, c, d, e) {
-    positionsArray.push(vertices[a]);
-    positionsArray.push(vertices[b]);
-    positionsArray.push(vertices[c]);
-    positionsArray.push(vertices[a]);
-    positionsArray.push(vertices[c]);
-    positionsArray.push(vertices[d]);
-    positionsArray.push(vertices[a]);
-    positionsArray.push(vertices[e]);
-    positionsArray.push(vertices[d]);
+  var xAxis = 0;
+  var yAxis = 1;
+  var zAxis = 2;
+  var axis = 0;
+  var theta = vec3(0, 0, 0);
 
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-    colorsArray.push(vertexColors[a]);
-  }
+  var thetaLoc;
+
+  var flag = false;
 
   init();
 
-  function colorDodecahedron() {
-    // Membentuk dodecahedron menggunakan pentagon()
-    // Setiap wajah pentagonal didefinisikan oleh 5 vertex dari vertices array
+  function quad(a, b, c, d) {
+    var t1 = subtract(vertices[b], vertices[a]);
+    var t2 = subtract(vertices[c], vertices[b]);
+    var normal = cross(t1, t2);
+    normal = vec3(normal);
 
-    dedocahedron(0, 16, 2, 10, 8); // Wajah 1
-    dedocahedron(0, 8, 4, 14, 12); // Wajah 2
-    dedocahedron(16, 17, 1, 12, 0); // Wajah 3
-    dedocahedron(1, 9, 11, 3, 17); // Wajah 4
-    dedocahedron(1, 12, 14, 5, 9); // Wajah 5
-    dedocahedron(2, 13, 15, 6, 10); // Wajah 6
-    dedocahedron(13, 3, 17, 16, 2); // Wajah 7
-    dedocahedron(3, 11, 7, 15, 13); // Wajah 8
-    dedocahedron(4, 8, 10, 6, 18); // Wajah 9
-    dedocahedron(14, 5, 19, 18, 4); // Wajah 10
-    dedocahedron(5, 19, 7, 11, 9); // Wajah 11
-    dedocahedron(15, 7, 19, 18, 6); // Wajah 12
+    positionsArray.push(vertices[a]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+    positionsArray.push(vertices[b]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+    positionsArray.push(vertices[c]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+    positionsArray.push(vertices[a]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+    positionsArray.push(vertices[c]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+    positionsArray.push(vertices[d]);
+    normalsArray.push(normal);
+    colorsArray.push(objectColor);
+  }
+
+  function colorCube() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
   }
 
   function init() {
@@ -127,32 +92,28 @@ var MidtermGrafkom = function () {
 
     gl = canvas.getContext("webgl2");
     if (!gl) alert("WebGL 2.0 isn't available");
-    
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-
     aspect = canvas.width / canvas.height;
 
+    gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 0.3);
 
     gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    colorDodecahedron();
+    colorCube();
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
-    var colorLoc = gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(colorLoc);
+    var normalLoc = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
@@ -162,57 +123,164 @@ var MidtermGrafkom = function () {
     gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
-    projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
 
-    // buttons for viewing parameters
+    var colorLoc = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colorLoc);
 
-    document.getElementById("Button1").onclick = function () {
-      near *= 1.1;
-      far *= 1.1;
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    viewerPos = vec3(0.0, 0.0, -20.0);
+
+    projectionMatrix = ortho(-aspect, aspect, -1, 1, -100, 100);
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    document.getElementById("ButtonX").onclick = function () {
+      axis = xAxis;
     };
-    document.getElementById("Button2").onclick = function () {
-      near *= 0.9;
-      far *= 0.9;
+    document.getElementById("ButtonY").onclick = function () {
+      axis = yAxis;
     };
-    document.getElementById("Button3").onclick = function () {
-      radius *= 2.0;
+    document.getElementById("ButtonZ").onclick = function () {
+      axis = zAxis;
     };
-    document.getElementById("Button4").onclick = function () {
-      radius *= 0.5;
+    document.getElementById("ButtonT").onclick = function () {
+      flag = !flag;
     };
-    document.getElementById("Button5").onclick = function () {
-      theta += dr;
+
+    document.getElementById("ambientColor").oninput = function (event) {
+      var lightAmbient = hexToRGBA(event.target.value);
+      ambientProduct = mult(lightAmbient, materialAmbient);
+      gl.uniform4fv(
+        gl.getUniformLocation(program, "uAmbientProduct"),
+        ambientProduct
+      );
     };
-    document.getElementById("Button6").onclick = function () {
-      theta -= dr;
+
+    document.getElementById("diffuseColor").oninput = function (event) {
+      var lightDiffuse = hexToRGBA(event.target.value);
+      diffuseProduct = mult(lightDiffuse, materialDiffuse);
+      gl.uniform4fv(
+        gl.getUniformLocation(program, "uDiffuseProduct"),
+        diffuseProduct
+      );
     };
-    document.getElementById("Button7").onclick = function () {
-      phi += dr;
+
+    document.getElementById("specularColor").oninput = function (event) {
+      var lightSpecular = hexToRGBA(event.target.value);
+      specularProduct = mult(lightSpecular, materialSpecular);
+      gl.uniform4fv(
+        gl.getUniformLocation(program, "uSpecularProduct"),
+        specularProduct
+      );
     };
-    document.getElementById("Button8").onclick = function () {
-      phi -= dr;
+
+    document.getElementById("lightX").oninput = function (event) {
+      lightPosition = event.target.value;
+      updateLightPosition();
     };
+    document.getElementById("lightY").oninput = function (event) {
+      lightPosition = event.target.value;
+      updateLightPosition();
+    };
+    document.getElementById("lightZ").oninput = function (event) {
+      lightPosition = event.target.value;
+      updateLightPosition();
+    };
+
+    gl.uniform4fv(
+      gl.getUniformLocation(program, "uAmbientProduct"),
+      ambientProduct
+    );
+    gl.uniform4fv(
+      gl.getUniformLocation(program, "uDiffuseProduct"),
+      diffuseProduct
+    );
+    gl.uniform4fv(
+      gl.getUniformLocation(program, "uSpecularProduct"),
+      specularProduct
+    );
+    gl.uniform4fv(
+      gl.getUniformLocation(program, "uLightPosition"),
+      lightPosition
+    );
+
+    gl.uniform1f(
+      gl.getUniformLocation(program, "uShininess"),
+      materialShininess
+    );
+
+    gl.uniformMatrix4fv(
+      gl.getUniformLocation(program, "uProjectionMatrix"),
+      false,
+      flatten(projectionMatrix)
+    );
+
+    window.addEventListener("resize", function () {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      aspect = canvas.width / canvas.height;
+      gl.viewport(0, 0, canvas.width, canvas.height);
+      projectionMatrix = ortho(-aspect, aspect, -1, 1, -100, 100);
+      gl.uniformMatrix4fv(
+        gl.getUniformLocation(program, "uProjectionMatrix"),
+        false,
+        flatten(projectionMatrix)
+      );
+    });
 
     render();
   }
 
+
   function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    eye = vec3(
-      radius * Math.sin(theta) * Math.cos(phi),
-      radius * Math.sin(theta) * Math.sin(phi),
-      radius * Math.cos(theta)
-    );
-    modelViewMatrix = lookAt(eye, at, up);
-    projectionMatrix = perspective(fovy, aspect, near, far);
+    if (flag) theta[axis] += 2.0;
 
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+    modelViewMatrix = mat4();
+    modelViewMatrix = mult(
+      modelViewMatrix,
+      rotate(theta[xAxis], vec3(1, 0, 0))
+    );
+    modelViewMatrix = mult(
+      modelViewMatrix,
+      rotate(theta[yAxis], vec3(0, 1, 0))
+    );
+    modelViewMatrix = mult(
+      modelViewMatrix,
+      rotate(theta[zAxis], vec3(0, 0, 1))
+    );
+
+    gl.uniformMatrix4fv(
+      gl.getUniformLocation(program, "uModelViewMatrix"),
+      false,
+      flatten(modelViewMatrix)
+    );
 
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+
     requestAnimationFrame(render);
+  }
+
+  function updateLightPosition() {
+    gl.uniform4fv(
+      gl.getUniformLocation(program, "uLightPosition"),
+      lightPosition
+    );
+  }
+
+  function hexToRGBA(hex) {
+    let r = parseInt(hex.slice(1, 3), 16) / 255;
+    let g = parseInt(hex.slice(3, 5), 16) / 255;
+    let b = parseInt(hex.slice(5, 7), 16) / 255;
+    return vec4(r, g, b, 1.0); // Return RGBA as vec4
   }
 };
 MidtermGrafkom();
